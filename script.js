fe1d362fdfea4eaade877efc805c013f26f38b2d
Pedro Ortiz <pedrops.ortiz@gmail.com>
2021-02-14 10:13:21 -0300

    Deleted the anterior code because wasn't working well

--- script.js
+++ script.js
@@ -3,16 +3,6 @@ window.onload = function(){
     var stage = document.getElementById("stage");
     var context = stage.getContext("2d");
 
-    context.fillStyle = "white";
-    context.fillRect(0,120,stage.width,20);
-
-    context.fillStyle = "white";
-    context.fillRect(0,262,stage.width,20);
-
-    context.fillStyle = "white";
-    context.fillRect(120,0,20,stage.height);
-
-    context.fillStyle = "white";
-    context.fillRect(262,0,20,stage.height);
+   
 
 }
